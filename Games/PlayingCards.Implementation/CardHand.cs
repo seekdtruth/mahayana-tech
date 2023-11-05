5af7655c77f49abdcdfd29217381e07af48badd1
seekdtruth <seekdtruth832@gmail.com>
2023-11-05 14:18:26 -0700

    Create CardHand.cs

---
+++ Games/PlayingCards.Implementation/CardHand.cs
@@ -0,0 +1,15 @@
+﻿namespace PlayingCards.Implementation;
+
+using PlayingCards.Core;
+
+public class CardHand : ICardHand
+{
+    private readonly List<ICard> cards = new();
+
+    public CardHand(params ICard[] cards)
+    {
+        this.cards = cards.ToList();
+    }
+
+    public IEnumerable<ICard> Cards => this.cards;
+}
