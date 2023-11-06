7740b9a7ab1664f83228571a5f17ef95f4816e6c
Darin L <seekdtruth832@gmail.com>
2023-11-05 22:18:17 -0700

    Games core library (#4)
    * Create Games.Core.csproj
    
    * Create IPlayer.cs
    
    * Create IGame.cs
    
    * Create Utilities.csproj
    
    * Create Extensions.cs
    
    * Update Games.Core.csproj
    
    * Update Games.sln
    
    * Create GameBase.cs

---
+++ Games/Games.Core/GameBase.cs
@@ -0,0 +1,44 @@
+﻿namespace Games.Core;
+
+using Utilities;
+
+public abstract class GameBase : IGame
+{
+    protected GameBase(params IPlayer[] players)
+    {
+        this.Players = !players.IsNullOrEmpty() ? new Queue<IPlayer>(players) : new Queue<IPlayer>();
+    }
+
+    /// <inheritdoc />
+    public Queue<IPlayer> Players { get; }
+
+    /// <inheritdoc />
+    public abstract void Start();
+
+    /// <inheritdoc />
+    public abstract void Initialize();
+
+    /// <inheritdoc />
+    public abstract void NextTurn();
+
+    /// <inheritdoc />
+    public abstract void NextRound();
+
+    /// <inheritdoc />
+    public abstract void EndRound();
+
+    /// <inheritdoc />
+    public abstract void ResetGame();
+
+    /// <inheritdoc />
+    public int CurrentRound
+    {
+        get; set;
+    }
+
+    /// <inheritdoc />
+    public int? MaxRounds
+    {
+        get; set;
+    }
+}
---
+++ Games/Games.Core/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿namespace Games.Core;
+
+public interface IPlayer
+{
+    /// <summary>
+    /// Player'sc current turn count.
+    /// </summary>
+    int TurnCount { get; set; }
+
+    /// <summary>
+    /// Player's current points.
+    /// </summary>
+    int Points { get; set; }
+}
